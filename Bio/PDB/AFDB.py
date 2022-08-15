e4ceb2b85f121ebb8ab1742571afdb761bc9512b
Sean Workman <sean.d.workman@gmail.com>
2022-08-15 17:02:31 -0600

    Added argparse for CLI argument handling. Added MANE download. Lots of bugs to fix.

--- Bio/PDB/AFDB.py
+++ Bio/PDB/AFDB.py
@@ -1,5 +1,6 @@
 """Access to AlphaFold predictions."""
 
+import argparse
 import contextlib
 import ftplib
 import json
@@ -8,6 +9,7 @@ import os
 import shutil
 import tarfile
 import tempfile
+from unicodedata import decomposition
 
 from urllib.request import urlopen
 from urllib.request import urlretrieve
@@ -130,7 +132,10 @@ class AFDB:
                     f"Bulk downloading AlphaFold2 predicted structures for {species} in {file_format} format..."
                 )
             ftp.retrbinary("RETR " + archive_file, tmpfile.write)
-            tarfile.open(os.path.abspath(tmpfile.name)).extractall(final_directory)
+            tar = tarfile.open(os.path.abspath(tmpfile.name))
+            tar.extractall(final_directory)
+
+        ftp.quit()
 
         if decompress:
             for model in os.listdir(final_directory):
@@ -172,8 +177,6 @@ class AFDB:
         else:
             path = pdir
         final_directory = os.path.join(path, f"swissprot_{file_format}")
-        if not os.path.isdir(final_directory):
-            os.mkdir(final_directory)
 
         # Skip download if the directory already exists
         if not overwrite:
@@ -184,6 +187,10 @@ class AFDB:
                         f"already exists at: {final_directory}."
                     )
                 return final_directory
+
+        # Make directory if it doesn't exist
+        if not os.path.exists(final_directory):
+            os.mkdir(final_directory)
 
         # Retrieve the tarball
         if self._verbose:
@@ -204,7 +211,10 @@ class AFDB:
                     "Swiss-Prot tarballs are >20 GB in size, this may take a while...\n"
                 )
             ftp.retrbinary("RETR " + archive[file_format], tmpfile.write)
-            tarfile.open(os.path.abspath(tmpfile.name)).extractall(final_directory)
+            tar = tarfile.open(os.path.abspath(tmpfile.name))
+            tar.extractall(final_directory)
+
+        ftp.quit()
 
         if decompress:
             for model in os.listdir(final_directory):
@@ -214,6 +224,61 @@ class AFDB:
                         shutil.copyfileobj(gz, final_file)
                     os.remove(file)
 
+        return final_directory
+
+    def retrieve_mane(self, pdir=None, overwrite=False, decompress=True):
+        """
+        Docstring needs to be completed.
+
+        Provides an interface to download MANE Select dataset.
+        """
+        # Where do the files get saved
+        if pdir is None:
+            path = self.local_afdb
+        else:
+            path = pdir
+        final_directory = os.path.join(path, "mane_select")
+
+        # Skip download if the directory already exists
+        if not overwrite:
+            if os.path.exists(final_directory):
+                if self._verbose:
+                    print(
+                        f"AlphaFold2 bulk download directory for MANE Select "
+                        f"already exists at: {final_directory}."
+                    )
+                return final_directory
+
+        # Make directory if it doesn't exist
+        if not os.path.exists(final_directory):
+            os.mkdir(final_directory)
+
+        # Retrieve the tarball
+        if self._verbose:
+            idx = self.afdb_server.find("://")
+        if idx >= 0:
+            ftp = ftplib.FTP(self.afdb_server[idx + 3 :])
+        else:
+            ftp = ftplib.FTP(self.afdb_server)
+        ftp.login()
+        ftp.cwd("/pub/databases/alphafold/latest/")
+
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            if self._verbose:
+                print("Bulk downloading AlphaFold2 predicted MANE Select structures...")
+            ftp.retrbinary("RETR mane_overlap_v3.tar", tmpfile.write)
+            tar = tarfile.open(os.path.abspath(tmpfile.name))
+            tar.extractall(final_directory)
+
+        ftp.quit()
+
+        if decompress:
+            for model in os.listdir(final_directory):
+                file = os.path.join(final_directory, model)
+                with gzip.open(file, "rb") as gz:
+                    with open(file[:-3], "wb") as final_file:
+                        shutil.copyfileobj(gz, final_file)
+                    os.remove(file)
         return final_directory
 
     def retrieve_prediction(
@@ -264,3 +329,109 @@ class AFDB:
             print(f"Download failed! Is {uniprot_id} the correct UniProt ID?")
 
         return final_file
+
+
+if __name__ == "__main__":
+
+    # Instantiate argument parser/subparser
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter, description="Placeholder"
+    )
+
+    # Set AFDB location
+    parser.add_argument(
+        "afdb_path",
+        nargs="?",
+        default=os.getcwd(),
+        help="local path to use, defaults to the current working directory",
+    )
+
+    # Add mutually exclusive group for modes
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument(
+        "-i",
+        "--identifiers",
+        metavar="uniprot id(s)",
+        help="retrieve predictions(s) - accepts a single uniprot id or a comma separated list of uniprot ids",
+    )
+    mode.add_argument(
+        "-m",
+        "--mane",
+        action="store_true",
+        help="bulk download AlphaFold2 predictions for the MANE Select dataset",
+    )
+    mode.add_argument(
+        "-s",
+        "--species",
+        metavar='"Species name"',
+        nargs=1,
+        help="bulk download AlphaFold2 predictions for provided species name (in quotation marks)",
+    )
+    mode.add_argument(
+        "-w",
+        "--swissprot",
+        metavar="format",
+        nargs=1,
+        help="bulk download AlphaFold2 predictions for SwissProt in a specified format",
+    )
+
+    # Add optional format argument that select choice
+    options = parser.add_argument_group()
+    options.add_argument(
+        "-d",
+        "--decompress",
+        choices=(True, False),
+        default=True,
+        help="decompress bulk downloaded predictions, defaults to True",
+    )
+    options.add_argument(
+        "-f",
+        "--format",
+        choices=("mmcif", "pdb"),
+        default="mmcif",
+        help="format in which to download predictions, defaults to mmcif",
+    )
+    options.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="overwrite existing prediction files",
+    )
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Set parameters
+    afdb_path = args.afdb_path
+    decompress = args.decompress
+    file_format = args.format
+    overwrite = args.overwrite
+
+    # Instantiate AFDB object
+    af = AFDB(afdb=afdb_path)
+
+    # Run
+    if args.identifiers:
+        uniprot_ids = args.identifiers.split(",")
+        for id in uniprot_ids:
+            af.retrieve_prediction(
+                id, pdir=afdb_path, file_format=file_format, overwrite=overwrite
+            )
+    elif args.mane:
+        af.retrieve_mane(pdir=afdb_path, overwrite=overwrite, decompress=decompress)
+    elif args.species:
+        species = args.species[0]
+        af.retrieve_species(
+            species,
+            pdir=afdb_path,
+            file_format=file_format,
+            overwrite=overwrite,
+            decompress=decompress,
+        )
+    elif args.swissprot:
+        af.retrieve_swissprot(
+            pdir=afdb_path,
+            file_format=file_format,
+            overwrite=overwrite,
+            decompress=decompress,
+        )
